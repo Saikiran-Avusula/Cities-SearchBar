7cda2e06ee59cd46d9d0283748ed79a21a5224d7
Saikiran-Avusula <saikiranavusula89@gmail.com>
2025-10-24 11:59:57 +0530

    refactor: change SearchInputController from @Controller to @RestController and update method signatures for clarity

--- city-searchBar-backEnd/src/main/java/com/example/city_SearchBar_backEnd/controller/SearchInputController.java
+++ city-searchBar-backEnd/src/main/java/com/example/city_SearchBar_backEnd/controller/SearchInputController.java
@@ -6,14 +6,15 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@Controller
+@RestController
 @RequestMapping("/cities")
 @CrossOrigin(origins = "http://localhost:5173")
 public class SearchInputController {
@@ -22,9 +23,8 @@ public class SearchInputController {
     @Autowired
     SearchInputService searchInputService;
 
-
     @GetMapping("/getAllCities")
-    public ResponseEntity<List<SearchInputEntity>> getAllSearchInputs(){
+    public ResponseEntity<List<SearchInputEntity>> getAllSearchInputs() {
         List<SearchInputEntity> searchInputEntityList = searchInputService.getAllSearchInput();
         System.out.println();
         log.info("---From controller---");
@@ -33,9 +33,9 @@ public class SearchInputController {
     }
 
     @GetMapping("/suggest")
-    public ResponseEntity<List<SearchInputEntity>> suggestCities(String keyword){
+    public ResponseEntity<List<SearchInputEntity>> suggestCities(@RequestParam(required = false) String keyword) {
         log.info("Received search keyword: {}", keyword);
-    List<SearchInputEntity> suggestions = searchInputService.searchCities(keyword);
-    return ResponseEntity.ok(suggestions);
+        List<SearchInputEntity> suggestions = searchInputService.searchCities(keyword);
+        return ResponseEntity.ok(suggestions);
     }
 }
